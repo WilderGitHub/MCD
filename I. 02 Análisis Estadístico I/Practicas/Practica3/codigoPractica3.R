c7d6db6e69dbbd526c2051dc8b792dc19e2928e0
WilderGithub <wilder_serdan@hotmail.com>
2021-11-26 06:50:06 -0400

    dos ejercicios de la practica 3

--- I. 02 Análisis Estadístico I/Practicas/Practica3/codigoPractica3.R
+++ I. 02 Análisis Estadístico I/Practicas/Practica3/codigoPractica3.R
@@ -1,6 +1,37 @@
-sd <- 5000
-mean<-36500
-x<-40000
+#Ejercicio 18 pag 242
+sd <- 8.2
+mean<-30
+
+# a)
+x<-40
+probZ<-pnorm(x,mean,sd,lower.tail = FALSE)
+#pMayorzZ<-1-probZ
+#pMayorzZ
+palmenos<-probZ
+palmenos
+# b)
+x<-20
 probZ<-pnorm(x,mean,sd,lower.tail = TRUE)
-pMayorzZ<-1-probZ
-pMayorzZ
+pAlomas<-probZ
+pAlomas
+# c)  
+probabilidad <- 0.9
+qnorm(probabilidad,mean,sd,lower.tail = TRUE)
+
+# Ejercicio 24 pag 242
+volumenes<-c(214,202,174,163,198,171,265,212,211,194,201,211,180)
+#a)
+media<-mean(volumenes)
+media
+ds<-sd(volumenes)
+ds
+#b)
+x<-180
+probZ<-pnorm(x,media,ds,lower.tail = FALSE)
+probMayor<-probZ
+probMayor
+
+# c)
+
+prob<-0.05
+qnorm(prob,media,ds,lower.tail = FALSE)
