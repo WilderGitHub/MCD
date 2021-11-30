6a541279166e6e64c1048adaa506fd4a3e264087
WilderGitHub <wilder_serdan@hotmail.com>
2021-11-30 17:02:55 -0400

    martes con practica 3 en elaboración

--- I. 02 Análisis Estadístico I/Practicas/Practica3/codigoPractica3.R
+++ I. 02 Análisis Estadístico I/Practicas/Practica3/codigoPractica3.R
@@ -39,17 +39,52 @@ qnorm(prob,media,ds,lower.tail = FALSE)
 # Distribuciones muestrales
 #Ejercicio 22 pag 278
 media<-51800
-n<-120
+n1<-30
+n2<-120
 desv<-4000
-error.est<-desv/sqrt(n)
+error.est1<-desv/sqrt(n1)
+error.est2<-desv/sqrt(n2)
 x1<-51300
 x2<-52300
+za<-(x1-media)/error.est1
+zb<-(x2-media)/error.est1
+1-(pnorm(zb)-pnorm(za))
+# grafico
+x <- seq(media-3*error.est1,media+3*error.est1 , by = 100)
+y1 <- dnorm(x, mean = media, sd = error.est1)
+y2 <- dnorm(x, mean = media, sd = error.est2)
+plot(x,y2)
+lines(x,y1)
+lines(x,y2, col="red",lty=2)
+
+# Ejercicio 26 pag 279
+
+media<-939
+n<-c(30,50,100,400)
+desv<-245
+error.est<-c(desv/sqrt(n))
+q<-25
+x1<-media -q
+x2<-media +q
 z1<-(x1-media)/error.est
 z2<-(x2-media)/error.est
 1-(pnorm(z2)-pnorm(z1))
+# grafico
 
-x <- seq(media-desv,media+desv , by = 100)
+x <- seq(media-3*error.est[1],media+3*error.est[1] , by = 1)
 y <- dnorm(x, mean = media, sd = error.est)
+
 plot(x,y)
+lines(x,y1, col="black",pch="o",lty=1)
+lines(x,y2, col="red",pch="*",lty=2)
+lines(x,y3, col="blue",pch="+",lty=3)
+lines(x,y4, col="green",pch="-",lty=4)
+
+legend(1,1,legend=c("y1","y2","y3","y4"), col=c("blue","red","black", "green"),
+pch=c("o","*","+","-"),lty=c(1,2,3,4), ncol=1)
+
+
+
+
 
 
