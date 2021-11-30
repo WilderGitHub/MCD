860672f003e3111af047b87a4f61593e48622a00
WilderGithub <wilder_serdan@hotmail.com>
2021-11-30 06:47:11 -0400

    sigo con la practica 3

--- I. 02 Análisis Estadístico I/Practicas/Practica3/codigoPractica3.R
+++ I. 02 Análisis Estadístico I/Practicas/Practica3/codigoPractica3.R
@@ -35,3 +35,21 @@ probMayor
 
 prob<-0.05
 qnorm(prob,media,ds,lower.tail = FALSE)
+
+# Distribuciones muestrales
+#Ejercicio 22 pag 278
+media<-51800
+n<-120
+desv<-4000
+error.est<-desv/sqrt(n)
+x1<-51300
+x2<-52300
+z1<-(x1-media)/error.est
+z2<-(x2-media)/error.est
+1-(pnorm(z2)-pnorm(z1))
+
+x <- seq(media-desv,media+desv , by = 100)
+y <- dnorm(x, mean = media, sd = error.est)
+plot(x,y)
+
+
