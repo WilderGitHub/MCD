343b8f241f18126c72251656a3c8d3d6d23d6467
WilderGitHub <wilder_serdan@hotmail.com>
2021-12-01 16:26:04 -0400

    falta poco para la pratica 3

--- I. 02 Análisis Estadístico I/Practicas/Practica3/codigoPractica3.R
+++ I. 02 Análisis Estadístico I/Practicas/Practica3/codigoPractica3.R
@@ -31,15 +31,12 @@ probZ<-pnorm(x,media,ds,lower.tail = FALSE)
 probMayor<-probZ
 probMayor
 
-# c)
 
-prob<-0.05
-qnorm(prob,media,ds,lower.tail = FALSE)
 
 # Distribuciones muestrales
 #Ejercicio 22 pag 278
 media<-51800
-n1<-30
+n1<-60
 n2<-120
 desv<-4000
 error.est1<-desv/sqrt(n1)
@@ -85,13 +82,36 @@ lines(x,y4, col="green",pch="-",lty=4)
 legend(1,10,legend=c("y1","y2","y3","y4"), col=c("blue","red","black", "green"),
 lty=c(1,2,3,4), ncol=1)
 
-######\
-p<-0.6
-n<-c(30,100)
+# proporciones
+######\ Ejercicio 38 pag 285
+p<-0.56
+n<-c(400)
 #desv<-245
 error.est<-c(sqrt((p*(1-p)/n)))
 error.est
-w<-0.05
+w<-0.04
+x1<-p - w
+x2<-p + w
+z1<-(x1-p)/error.est
+z2<-(x2-p)/error.est
+pnorm(z2)-pnorm(z1)
+# grafico
+x <- seq(p-3*error.est[1],p+3*error.est[1] , by = .01)
+y <- dnorm(x, mean = p, sd = error.est)
+y1 <- dnorm(x, mean = p, sd = error.est[1])
+y2 <- dnorm(x, mean = p, sd = error.est[2])
+
+plot(x,y)
+lines(x,y1, col="black",pch="o",lty=1)
+lines(x,y2, col="red",pch="*",lty=2)
+
+######\ Ejercicio 40 pag 285
+p<-0.76
+n<-c(400)
+#desv<-245
+error.est<-c(sqrt((p*(1-p)/n)))
+error.est
+w<-0.03
 x1<-p - w
 x2<-p + w
 z1<-(x1-p)/error.est
@@ -99,10 +119,10 @@ z2<-(x2-p)/error.est
 pnorm(z2)-pnorm(z1)
 # grafico
 
-x <- seq(media-3*error.est[1],media+3*error.est[1] , by = .001)
-y <- dnorm(x, mean = media, sd = error.est)
-y1 <- dnorm(x, mean = media, sd = error.est[1])
-y2 <- dnorm(x, mean = media, sd = error.est[2])
+x <- seq(p-3*error.est[1],p+3*error.est[1] , by = .001)
+y <- dnorm(x, mean = p, sd = error.est)
+y1 <- dnorm(x, mean = p, sd = error.est[1])
+y2 <- dnorm(x, mean = p, sd = error.est[2])
 
 #y3 <- dnorm(x, mean = media, sd = error.est[3])
 #y4 <- dnorm(x, mean = media, sd = error.est[4])
@@ -110,8 +130,32 @@ plot(x,y)
 lines(x,y1, col="black",pch="o",lty=1)
 lines(x,y2, col="red",pch="*",lty=2)
 
-# vas a sacar ejercicio de la pagina 283 y mas
+#### ejercicio 14 pag 314
 
+media<-22.5
+n<-54
+desv<-4.4
+NConfianza<-c(0.90,0.91, 0.92, 0.93,0.94,0.95,0.96,0.97,0.98,0.99)
+Z<-qnorm((1-NConfianza)/2,lower.tail = FALSE)
+error.est <- desv/sqrt(n)
+margen.error <- Z * error.est
+lim.inf <- media - margen.error#
+lim.sup <- media + margen.error#
+Medida<-c("Nivel de Confianza","Limite Inferior","Limite Superior")
+tabla = data.frame(NConfianza,lim.inf,lim.sup)
+tabla  
 
-
-
+#### ejercicio 17 pag 315
+install.packages("readxl")
+library("readxl")
+miami <- read_excel ("miami.xlsx")
+media<-mean(miami$calificaciones)
+n<-length(miami$calificaciones)
+desv<-sd(miami$calificaciones)
+NConfianza<-c(0.95)
+Z<-qnorm((1-NConfianza)/2,lower.tail = FALSE)
+error.est <- desv/sqrt(n)
+margen.error <- Z * error.est
+lim.inf <- media - margen.error
+lim.sup <- media + margen.error
+cat("limite inferior: ", lim.inf, ", limite superior: ",lim.sup)
